9a99384f0d359f95ce35356eedc981ac115d2ce3
koch3092 <developer@dorakoch.com>
2025-03-18 19:33:54 +0800

    refactor: clean unused code

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,8 +1,4 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, users, tasks
 
 api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
